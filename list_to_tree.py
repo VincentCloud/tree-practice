57440b35135ed028985d476160e7820dffdd0263
Vincent Huang <chenzhun.huang@mail.mcgill.ca>
2019-10-31 21:01:50 -0400

    .gitignore:

--- list_to_tree.py
+++ list_to_tree.py
@@ -3,10 +3,11 @@ from tree_node import TreeNode
 
 class ConvertTree:
     def convert_tree(self, l: list) -> TreeNode:
-        root = TreeNode
+        root = TreeNode(l[0])
         front = 0
         index = 1
 
+        while index < len()
 
 
 
